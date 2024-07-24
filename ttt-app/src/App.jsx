b0753a57c6cf10df14d22943563f9bfc179e5fcd
freecatdog <128010783+freecatdog@users.noreply.github.com>
2024-07-24 17:04:45 +0900

    eunsu-충돌 해결 및 수정사항 커밋

--- ttt-app/src/App.jsx
+++ ttt-app/src/App.jsx
@@ -5,12 +5,11 @@ import Blog from "./pages/Blog";
 import Auth from "./pages/Auth";
 import SearchPage from "./pages/SearchPage";
 import AddPost from "./components/BlogEditer/AddPost";
-
 import Profile from "./pages/Profile";
 import SignUp from "./components/User/SignUp";
 import SignIn from "./components/User/SignIn";
-//import ProfileCard from "./components/User/ProfileCard";
 import MapHome from "./components/Map/MapHome"; // MapHome 컴포넌트 import
+import Terms from "./components/User/Terms"; // Terms 컴포넌트 import
 
 function App() {
   const [user, setUser] = useState(null); // 사용자 상태 정의
@@ -18,6 +17,7 @@ function App() {
   const handleSignUp = (newUser) => {
     setUser(newUser); // 새로운 사용자 설정
   };
+
   const handleSignIn = (user) => {
     setUser(user); // 사용자 로그인 설정
   };
@@ -40,7 +40,7 @@ function App() {
               element={user ? <MapHome /> : <Navigate to="/signin" />} // 사용자 로그인 시 지도 페이지로 이동
             />
             <Route path="/profile" element={user ? <MapHome /> : <Profile />} />{" "}
-            {/*사용자 로그인 시 지도 페이지로 이동 */}
+            {/* 사용자 로그인 시 지도 페이지로 이동 */}
             <Route
               path="/signup"
               element={<SignUp onSignUp={handleSignUp} />}
@@ -58,6 +58,8 @@ function App() {
               }
             />{" "}
             {/* SignIn 라우트에 onSignIn 핸들러 수정: 로그인 후 /home으로 이동 */}
+            <Route path="/terms/:type" element={<Terms />} />{" "}
+            {/* Terms 컴포넌트에 대한 라우트 추가 */}
           </Routes>
         </BrowserRouter>
       </div>
