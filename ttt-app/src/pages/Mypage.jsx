21d3db20f5b31fe9f5f11ece7ef1a6640c4173c7
eoyan <nabong1031@gmail.com>
2024-07-26 21:31:37 +0900

    design: Mypage 프로필 우측 하단에 프로필 수정 버튼 절대 위치로 고정

--- ttt-app/src/pages/Mypage.jsx
+++ ttt-app/src/pages/Mypage.jsx
@@ -1,25 +1,20 @@
-import ProfileCard from './../components/User/ProfileCard';
-import './Mypage.css'
-import btnimg from '../assets/Button-Edit.png';
-
-
+import ProfileCard from "./../components/User/ProfileCard";
+import "./Mypage.css";
+import btnimg from "../assets/Button-Edit.png";
 
 const Mypage = () => {
-
-
-    return (
-        <div>
-            <div className = "profile-container">
-                <ProfileCard/>
-            </div>
-            <div className = "edit-button-container">
-                <button className = "edit-button">
-                    <img src={btnimg}/>
-                </button>
-            </div>
+  return (
+    <div>
+      <div className="profile-container">
+        <ProfileCard />
+        <div className="edit-button-container">
+          <button className="edit-button">
+            <img src={btnimg} />
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Mypage;
-
