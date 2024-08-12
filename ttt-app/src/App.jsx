36f0c294ddc1fe353985f98e771dd05fc4a65222
Karmesin924 <jinhong0924@naver.com>
2024-08-12 19:08:14 +0900

    feat: PostDetail.jsx (세부 글 페이지) 추가 및 Route 설정

--- ttt-app/src/App.jsx
+++ ttt-app/src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
-import Home from "./pages/Home";
-import Blog from "./pages/Blog";
-import Auth from "./pages/Auth";
-import SearchPage from "./pages/SearchPage";
-import AddPost from "./pages/AddPost";
-import Profile from "./pages/Profile";
-import SignUp from "./components/User/SignUp";
-import SignIn from "./components/User/SignIn";
-import MapHome from "./components/Map/MapHome"; // MapHome 컴포넌트 import
-import Terms from "./components/User/Terms"; // Terms 컴포넌트 import
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Auth from './pages/Auth';
+import SearchPage from './pages/SearchPage';
+import AddPost from './pages/AddPost';
+import Profile from './pages/Profile';
+import SignUp from './components/User/SignUp';
+import SignIn from './components/User/SignIn';
+import MapHome from './components/Map/MapHome'; // MapHome 컴포넌트 import
+import Terms from './components/User/Terms'; // Terms 컴포넌트 import
 import Mypage from './pages/Mypage';
+import PostDetail from './pages/PostDetail';
 
 function App() {
   const [user, setUser] = useState(null); // 사용자 상태 정의
@@ -35,19 +36,19 @@ function App() {
             <Route path="/auth" element={<Auth />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/addpost" element={<AddPost />} />
+            <Route path="/post/:id" element={<PostDetail />} />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/mypage" element={<Mypage />} />
-            
             <Route
               path="/map"
               element={user ? <MapHome /> : <Navigate to="/signin" />} // 사용자 로그인 시 지도 페이지로 이동
             />
-            <Route path="/profile" element={user ? <MapHome /> : <Profile />} />{" "}
+            <Route path="/profile" element={user ? <MapHome /> : <Profile />} />{' '}
             {/* 사용자 로그인 시 지도 페이지로 이동 */}
             <Route
               path="/signup"
               element={<SignUp onSignUp={handleSignUp} />}
-            />{" "}
+            />{' '}
             {/* SignUp 라우트에 onSignUp 핸들러 추가 */}
             <Route
               path="/signin"
@@ -55,13 +56,13 @@ function App() {
                 <SignIn
                   onSignIn={(user) => {
                     handleSignIn(user);
-                    window.location.href = "/home";
+                    window.location.href = '/home';
                   }}
                 />
               }
-            />{" "}
+            />{' '}
             {/* SignIn 라우트에 onSignIn 핸들러 수정: 로그인 후 /home으로 이동 */}
-            <Route path="/terms/:type" element={<Terms />} />{" "}
+            <Route path="/terms/:type" element={<Terms />} />{' '}
             {/* Terms 컴포넌트에 대한 라우트 추가 */}
           </Routes>
         </BrowserRouter>
---
+++ ttt-app/src/pages/PostDetail.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar/Navbar';
+
+const PostDetail = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default PostDetail;
