f3a66b65364e404926e6b6768d6c4ade9663c5b0
Karmesin924 <jinhong0924@naver.com>
2024-08-16 13:54:00 +0900

    feat: PostDetail.jsx - post State 데이터 & 더미데이터 추가

--- ttt-app/src/pages/PostDetail.jsx
+++ ttt-app/src/pages/PostDetail.jsx
@@ -14,10 +14,13 @@ const PostDetail = () => {
   const { id } = useLocation();
   const [post, setPost] = useState({
     id: null,
-    username: '',
     title: '',
+    username: '',
+    profileImageUrl: '',
+    introduction: '',
     data: '',
     locations: [],
+    postCount: 0,
     likeCount: 0,
     viewCount: 0,
     commentCount: 0,
@@ -84,14 +87,17 @@ const PostDetail = () => {
 
   const dummyPost = {
     id: 1,
-    username: '천재123',
     title: '오늘은 석계문고에 다녀왔습니다.',
+    username: '천재123',
+    profileImageUrl: 'https://example.com/path/to/profile-image.jpg',
+    introduction: '저는 여행다니기를 좋아하는 사람입니다. 만나서 반갑습니다.',
     data: `<p>요즘 여러 신축 건물에 위치한 예쁜 서점들을 자주 들르는 편인데, <br><br><br><br><br>테라코타 벽돌을 테마로 그에 맞춰 아름다운 조형물을 내세우거나 아예 고풍스러운 카페를 콘셉트로 잡은 후 샹들리에를 달고 은촛대를 놓은 모습을 보면 저도 모르게 만족감이 느껴집니다. 하지만 그러면서도, 어릴 적 많이 볼 수 있었던 지하 잡화점이나 문구점, 서점이 그리워질 때가 있습니다...</p>
-    <p>지하는 사실 그렇게 선호되는 건물 양식은 아닙니다. 습해서 곰팡이가 잘 피기 쉽상이고, 여름엔 더 덥고 겨울엔 더 추우니까요. <br><br><br><br><br><br><br><br><br><br><br><br>그래서 그런지 지하에 위치한 상점을 보기가 요 근래에는 더 힘들어진 듯합니다.</p>`,
+  <p>지하는 사실 그렇게 선호되는 건물 양식은 아닙니다. 습해서 곰팡이가 잘 피기 쉽상이고, 여름엔 더 덥고 겨울엔 더 추우니까요. <br><br><br><br><br><br><br><br><br><br><br><br>그래서 그런지 지하에 위치한 상점을 보기가 요 근래에는 더 힘들어진 듯합니다.</p>`,
     locations: [
       { id: 1, name: '석계문고' },
       { id: 2, name: '또 다른 장소' },
     ],
+    postCount: 5,
     likeCount: 53,
     commentCount: 12,
     viewCount: 123,
