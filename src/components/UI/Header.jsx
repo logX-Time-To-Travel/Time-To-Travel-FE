ea1b22a25aeef8a22c804861106282b0ca57a8a4
eoyan <nabong1031@gmail.com>
2024-08-30 11:00:45 +0900

    design: Header의 3가지 요소를 각각 좌우와 중앙 정렬 배치

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,26 +1,28 @@
-import { useNavigate } from "react-router-dom";
-import "./Header.css";
-import backicon from "../../assets/Icon_ Back 1.png";
+import { useNavigate } from 'react-router-dom';
+import './Header.css';
+import backicon from '../../assets/Icon_ Back 1.png';
 
-const Header = ({ text, rightText}) => {
+const Header = ({ text, rightText, customFunc }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate("/");
+    navigate('/');
   };
 
   return (
     <header className="Header">
       <div className="homebutton_section">
         <button onClick={handleClick} className="home-button">
-          <img src={backicon} /> 
+          <img src={backicon} />
         </button>
       </div>
       <div className="title">
         <b>{text}</b>
       </div>
-      <div className = "rightbutton_section">
-        <b>{rightText}</b>
+      <div className="rightbutton_section">
+        <button onClick={customFunc}>
+          <b>{rightText}</b>
+        </button>
       </div>
     </header>
   );
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -12,7 +12,14 @@ const Blog = () => {
     <div>
       <div className="page-container">
         <div className="blog-container">
-          <Header homebtn={' '} text={'내 작성 게시글'} />
+          <Header
+            homebtn={' '}
+            text={'내 작성 게시글'}
+            rightText={'선택모드'}
+            customFunc={() => {
+              console.log('녕안');
+            }}
+          />
           <PostList posts={posts} />
         </div>
       </div>
