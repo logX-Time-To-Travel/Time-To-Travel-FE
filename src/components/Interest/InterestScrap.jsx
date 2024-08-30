8abbe7ff9c97075e8db41ef4e7834b9ebbb603c7
Karmesin924 <jinhong0924@naver.com>
2024-08-30 16:07:57 +0900

    feat: 관심사 페이지 - 스크랩 컴포넌트 구현, 디자인

---
+++ src/components/Interest/InterestScrap.jsx
@@ -0,0 +1,12 @@
+import './InterestScrap.css';
+
+const InterestScrap = () => {
+  return (
+    <div className="interest-scrap-container">
+      <div className="scrap-title">내가 별표한 게시글</div>
+      <div className="scrap-count">2개</div>
+    </div>
+  );
+};
+
+export default InterestScrap;
