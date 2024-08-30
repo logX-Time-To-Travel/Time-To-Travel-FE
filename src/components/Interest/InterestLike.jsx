f245cd7103a2f2224f868edff651dc23db68d1f6
Karmesin924 <jinhong0924@naver.com>
2024-08-30 16:07:15 +0900

    feat: 관심사 페이지 - 좋아요 컴포넌트 구현, 디자인

---
+++ src/components/Interest/InterestLike.jsx
@@ -0,0 +1,12 @@
+import './InterestLike.css';
+
+const InterestLike = () => {
+  return (
+    <div className="interest-heart-container">
+      <div className="heart-title">내가 좋아요한 게시글</div>
+      <div className="heart-count">0개</div>
+    </div>
+  );
+};
+
+export default InterestLike;
